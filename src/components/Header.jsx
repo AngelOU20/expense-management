5f8a2f0b45820b0517867b9b452476bf2078a3a3
Julio Ucharima <julio_ucharima@usmp.pe>
2024-05-28 17:39:45 -0500

    implement Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <h1 className="text-4xl font-extrabold text-sinbad text-center">
+      ¿Cuánto <span className="text-eden">dinero</span> necesitas?
+    </h1>
+  );
+};
